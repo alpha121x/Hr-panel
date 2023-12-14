4a5a38fc0d421fba3c52b83c6eec0ab200ac24e2
Abbasch <109143082+alpha121x@users.noreply.github.com>
2023-12-14 15:19:04 +0500

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -2,7 +2,7 @@
 require_once "include/classes/meekrodb.2.3.class.php";
 DB::$user = 'root';
 DB::$password = '';
-DB::$dbName = 'arshaphp';
+DB::$dbName = 'bixisoft';
 DB::$host = 'localhost';
 
 // Test the connection
