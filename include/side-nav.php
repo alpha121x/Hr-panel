3faed1be5dfc088a8c553ef102fc242d01b84f49
Abbasch <109143082+alpha121x@users.noreply.github.com>
2023-12-14 15:16:45 +0500

    Update side-nav.php

--- include/side-nav.php
+++ include/side-nav.php
@@ -15,58 +15,10 @@ if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 'admin') {
          <li class="nav-item">
            <a class="nav-link collapsed"  href="add-user-profile.php">
            <i class="bi bi-person-fill-add"></i>
-             <span>Add Users Profile</span>
+             <span></span>
            </a>
          </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="admin-profile.php">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Admin Profile</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="admin_users.php">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Users</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="users_address.php">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Users Address</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="add_user_address.php">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Add Users Address</span>
-           </a>
-         </li>
-   </aside><!-- End Sidebar-->';
-} else {
-    echo'<!-- ======= Sidebar ======= -->
-    <aside id="sidebar" class="sidebar">
-   
-   <ul class="sidebar-nav" id="sidebar-nav">
-   
-     <li class="nav-item">
-       <a class="nav-link " href="index.php">
-       <i class="bi bi-journal-text"></i>
-         <span>Dashboard</span>
-       </a>
-     </li><!-- End Dashboard Nav --> 
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="admin_users.php">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Users</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="users_address.php">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Users Address</span>
-           </a>
-         </li>
+        
    </aside><!-- End Sidebar-->';
 }
 ?>
