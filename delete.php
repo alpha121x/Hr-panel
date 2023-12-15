9fde562328fe1cd350114cc9c2410959bcc14bef
Abbasch <109143082+alpha121x@users.noreply.github.com>
2023-12-16 00:57:01 +0500

    Revert "changed repoitory structure.."
    This reverts commit 829c5cae360463b1f29c4fd0bb4eb6b7e48a7f23.

---
+++ delete.php
@@ -0,0 +1,41 @@
+<?php
+include("db_config.php");
+
+// Check if user ID is provided in the URL
+if (isset($_GET['deleteid'])) {
+    $user_id = $_GET['deleteid'];
+
+    // Delete query using MeekroDB
+    $deleted = DB::delete('admin_users', 'user_id=%i', $user_id);
+
+    if ($deleted) {
+        header("Location: admin_users.php");
+    } else {
+        header("Location: admin_users.php");
+    }
+} else {
+    // Handle the case where no user ID is provided in the URL
+    header("Location: admin_users.php");
+}
+?>
+
+<?php
+include("db_config.php");
+
+// Check if user ID is provided in the URL
+if (isset($_GET['deleteid'])) {
+    $user_id = $_GET['deleteid'];
+
+    // Delete query using MeekroDB
+    $deleted = DB::delete('users_address', 'user_id=%i', $user_id);
+
+    if ($deleted) {
+        header("Location: users_address.php");
+    } else {
+        header("Location: users_address.php");
+    }
+} else {
+    // Handle the case where no user ID is provided in the URL
+    header("Location: users_address.php");
+}
+?>
