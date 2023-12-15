1201b28cc17095f300ba48b98c8db6951c332e69
Abbasch <109143082+alpha121x@users.noreply.github.com>
2023-12-15 20:45:20 +0500

    changes

--- attendance-daily.php
+++ attendance-daily.php
@@ -23,19 +23,11 @@
             <div class="row m-0">
                 <div class="col-md-3">
                     <div class="mb-3">
-                        <label for="email" class="form-label">Departments</label>
+                        <label for="email" class="form-label">Employees</label>
                         <div class="col-sm-9">
-                            <select id="inputState" class="form-control form-select" required name="department">
-                                <option selected>Departments</option>
-                                <option value="Call Center">Call Center</option>
-                                <option value="WordPress">WordPress</option>
-                                <option value="PHP Developer">PHP Developer</option>
-                                <option value="Designing">Designing</option>
-                                <option value="Marketing">Marketing</option>
+                            <select id="" class="form-control form-select" required name="employee">
+                                <option value=""></option>
                             </select>
-                            <div class="valid-feedback">
-                                Please Enter Your Department
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -80,30 +72,6 @@
                 </div> -->
 
             </div>
-            <br>
-            <div class="row m-0">
-                <div class="container mt-3">
-                    <!-- <h2 class="text-center">Display Test Db Data</h2> -->
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <td>ID</td>
-                                <td>Employe name</td>
-                                <td>Phone Number</td>
-                                <td>Attendance</td>
-                            </tr>
-                        </thead>
-                        <tbody id="load-data-into-tbody">
-                            <tr>
-                                <td class="text-center text-warning" colspan="4">
-                                    Click on get employyes button....
-                                </td>
-                            </tr>
-                        </tbody>
-
-                    </table>
-                </div>
-            </div>
 
 
 
@@ -116,30 +84,6 @@
     <?php include "include/footer.php" ?>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Script for load data into table -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-    <script>
-    $(document).ready(function () {
-        // Define the click event for the button
-        $('#btn').click(function () {
-            // Make an AJAX request when the button is clicked
-            $.ajax({
-                url: "load-employees-data.php",
-                type: 'POST',
-                success: function (data) {
-                    // Update the content or perform any action here
-                    $('#load-data-into-tbody').html(data);
-                    console.log("Data loaded successfully!");
-                },
-                error: function (xhr, status, error) {
-                    console.error(xhr.responseText);
-                }
-            });
-        });
-    });
-</script>
 
     <?php include "include/script-files.php" ?>
 
